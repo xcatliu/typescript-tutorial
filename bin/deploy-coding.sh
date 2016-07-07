ddbdecd4c675121fe1ba7017b3b0e6b1a69ddb07
xcatliu <xcatliu@gmail.com>
2016-07-07 20:52:13 +0800

    Add travis

---
+++ bin/deploy-coding.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+set -e
+
+rm -rf coding-repo
+git clone git@git.coding.net:xcatliu/from-javascript-to-typescript.git coding-repo
+cd coding-repo
+
+git checkout coding-pages
+
+rsync -a ../_book ./
+
+git add -A
+git commit -m 'Update coding-pages'
+git push origin coding-pages
+
+cd ..
+rm -rf coding-repo
+
