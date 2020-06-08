0cf0b0063070534bb2e3698a460be73cc3c9c0b2
xcatliu <xcatliu@users.noreply.github.com>
2020-06-08 03:06:55 +0000

    deploy: d6979e156fa047318d956ebd882a91694a405b0c

--- _layout.js
+++ _layout.js
@@ -4,8 +4,8 @@ import Sidebar from './_sidebar.js';
 const Layout = ({ config, title, content, script, sidebar, outputPath }) => {
     const [isDark, setIsDark] = React.useState(
     // @ts-ignore
-    !!(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches));
-    return (React.createElement("html", { className: isDark ? 'dark' : '' },
+    typeof Deno === 'undefined' ? document.documentElement.classList.contains('is_dark') : false);
+    return (React.createElement("html", { className: isDark ? 'is_dark' : '' },
         React.createElement("head", null,
             React.createElement("script", { async: true, src: "https://www.google-analytics.com/analytics.js" }),
             React.createElement("script", { dangerouslySetInnerHTML: {
@@ -14,8 +14,17 @@ const Layout = ({ config, title, content, script, sidebar, outputPath }) => {
             React.createElement("title", null, outputPath !== 'index.html' ? `${title} · ${config.title}` : title),
             React.createElement("meta", { charSet: "utf-8" }),
             React.createElement("link", { rel: "shortcut icon", type: "image/png", href: "//xcatliu.github.io/favicon.ico" }),
-            React.createElement("link", { rel: "stylesheet", href: isDark ? '/assets/prism_tomorrow.css' : '/assets/prism.css' }),
-            React.createElement("link", { rel: "stylesheet", href: "/assets/index.css" })),
+            React.createElement("link", { id: "prismTheme", rel: "stylesheet", href: isDark ? '/assets/prism_tomorrow.css' : '/assets/prism.css' }),
+            React.createElement("link", { rel: "stylesheet", href: "/assets/index.css" }),
+            React.createElement("script", { dangerouslySetInnerHTML: {
+                    __html: `
+let shouldSetIsDark = document.cookie.includes('is_dark=1') ? true : document.cookie.includes('is_dark=0') ? false : window.matchMedia('(prefers-color-scheme: dark)').matches
+if (shouldSetIsDark) {
+  document.documentElement.classList.add('is_dark');
+  document.getElementById('prismTheme').href = "/assets/prism_tomorrow.css";
+}
+`
+                } })),
         React.createElement("body", null,
             React.createElement("header", null,
                 React.createElement("h1", null,
@@ -32,6 +41,8 @@ const Layout = ({ config, title, content, script, sidebar, outputPath }) => {
                             React.createElement("a", { href: "#", onClick: (e) => {
                                     e.preventDefault();
                                     setIsDark(!isDark);
+                                    // @ts-ignore
+                                    document.cookie = `is_dark=${!isDark ? '1' : '0'}; expires=Tue, 19 Jun 2038 03:14:07 UTC; path=/`;
                                 } }, isDark ? '关闭黑暗模式' : '开启黑暗模式'))))),
             React.createElement(Sidebar, { sidebar: sidebar, outputPath: outputPath }),
             React.createElement("section", { className: "main" }, content),
