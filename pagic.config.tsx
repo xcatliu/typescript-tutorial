8a8dda490089b269daaa2fa3a435ebe5a42977a1
xcatliu <xcatliu@gmail.com>
2020-08-10 13:20:14 +0800

    fix: ci

--- pagic.config.tsx
+++ pagic.config.tsx
@@ -1,4 +1,4 @@
-// @deno-types="https://deno.land/x/types/react/v16.13.1/react.d.ts"
+// @deno-types="https://deno.land/x/pagic@v0.8.4/src/types/react/v16.13.1/react.d.ts"
 import React from 'https://dev.jspm.io/react@16.13.1';
 
 export default {
