18dfbf904b48e27b76c07f389f1ea0b244b00cb7
xcatliu <xcatliu@users.noreply.github.com>
2023-06-26 02:38:42 +0000

    deploy: d665077a0b57e1db81f6fac22b871a968a968c2b

--- pagic.config.js
+++ pagic.config.js
@@ -4,7 +4,7 @@ export default {
     theme: 'docs',
     plugins: ['sidebar', 'prev_next', 'gitalk', 'ga'],
     title: 'TypeScript 入门教程',
-    description: '从 JavaScript 程序员的角度总结思考，循序渐进的理解 TypeScript。',
+    description: '从 JavaScript 程序员的角度总结思考，循序渐进的理解 TypeScript',
     github: 'https://github.com/xcatliu/typescript-tutorial',
     head: React.createElement("link", { rel: "icon", type: "image/png", href: "/favicon.png" }),
     nav: [
